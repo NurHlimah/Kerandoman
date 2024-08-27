a89b0929b338c964b369e5cd4949f978afa7bb30
NurH <146801977+NurHlimah@users.noreply.github.com>
2024-08-27 11:33:50 +0700

    Jump and assets

--- Assets/Script/Player_Movement.cs
+++ Assets/Script/Player_Movement.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class Player_Movement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float jumpHeight = 10f;
+    [SerializeField] private LayerMask groundLayer;
     private Rigidbody2D _rbody;
     private float _horizontalMovement;
+    private bool _onTheGround;
 
     // Start is called before the first frame update
     void Start()
@@ -28,7 +31,9 @@ public class Player_Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _horizontalMovement = (Input.GetAxis("Horizontal"));
+        GroundCheck();
+        _horizontalMovement = Input.GetAxis("Horizontal");
+        Jump();
     }
 
     private void Awake()
@@ -43,5 +48,30 @@ public class Player_Movement : MonoBehaviour
     private void Move()
     {
         _rbody.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, _rbody.velocity.y);
+    }
+
+    private bool OnTheGround()
+    {
+        RaycastHit2D hit =
+            Physics2D.Raycast(transform.position, Vector2.down,2f, groundLayer);
+        return hit.collider != null;
+    }
+
+    private void GroundCheck()
+    {
+        RaycastHit2D[] hits = new RaycastHit2D[5];
+        int numhits = _rbody.Cast(Vector2.down, hits, 0.5f);
+        _onTheGround = numhits > 0;
+    }
+
+    private void Jump()
+    {
+        if(Input.GetButtonDown("Jump") && _onTheGround)
+        {
+            _rbody.AddForce(
+                 Vector2.up * jumpHeight,
+                 ForceMode2D.Impulse
+                );
+        }
     }
 }
